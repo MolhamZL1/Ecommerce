6661af415d3d3989df7c5493ee4c1c3e3e180acd
molhamsheikh <molhamsa49@gmail.com>
2024-09-14 20:10:50 +0300

    add alertExitApp

--- lib/view/widget/auth/LoginViewBody.dart
+++ lib/view/widget/auth/LoginViewBody.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce/controller/auth/signinController.dart';
+import 'package:ecommerce/core/functions/alertexitdialog.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -15,30 +16,35 @@ class LoginViewBody extends GetView<SignInControllerImp> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      padding: const EdgeInsets.symmetric(horizontal: 32),
-      children: [
-        const HeadLoginSection(),
-        const TextFieldsLoginSection(),
-        GestureDetector(
-            onTap: controller.goToForgetPassword,
-            child: const Text(
-              "Forget Password",
-              textAlign: TextAlign.end,
-              style: TextStyle(color: AppColor.grey),
-            )),
-        const SizedBox(
-          height: 10,
-        ),
-        CustomAuthButton(
-          text: "Sign In",
-          onPressed: () {},
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        const DontHaveAccountSection()
-      ],
+    return WillPopScope(
+      onWillPop: () {
+        return alertExitApp();
+      },
+      child: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        children: [
+          const HeadLoginSection(),
+          const TextFieldsLoginSection(),
+          GestureDetector(
+              onTap: controller.goToForgetPassword,
+              child: const Text(
+                "Forget Password",
+                textAlign: TextAlign.end,
+                style: TextStyle(color: AppColor.grey),
+              )),
+          const SizedBox(
+            height: 10,
+          ),
+          CustomAuthButton(
+            text: "Sign In",
+            onPressed: () {},
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const DontHaveAccountSection()
+        ],
+      ),
     );
   }
 }
