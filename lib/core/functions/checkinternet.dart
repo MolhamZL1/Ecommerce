a155559e1c1d8193aed33441070ba880acb05e20
molhamsheikh <molhamsa49@gmail.com>
2024-09-16 11:44:28 +0300

    retouch check internet

--- lib/core/functions/checkinternet.dart
+++ lib/core/functions/checkinternet.dart
@@ -1,11 +1,16 @@
 import 'dart:io';
 
 Future<bool> checkInternet() async {
-  List<InternetAddress> internetAddress =
-      await InternetAddress.lookup("google.com");
-  if (internetAddress.isNotEmpty && internetAddress[0].rawAddress.isNotEmpty) {
-    return true;
-  } else {
+  try {
+    List<InternetAddress> internetAddress =
+        await InternetAddress.lookup("google.com");
+    if (internetAddress.isNotEmpty &&
+        internetAddress[0].rawAddress.isNotEmpty) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (e) {
     return false;
   }
 }
