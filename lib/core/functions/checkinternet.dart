82dc7dc97ebdf7804f343de6008f86cd7cb20514
molhamsheikh <molhamsa49@gmail.com>
2024-09-16 10:52:33 +0300

    add check internet function and creat api class

--- lib/core/functions/checkinternet.dart
+++ lib/core/functions/checkinternet.dart
@@ -1,1 +1,11 @@
+import 'dart:io';
 
+Future<bool> checkInternet() async {
+  List<InternetAddress> internetAddress =
+      await InternetAddress.lookup("google.com");
+  if (internetAddress.isNotEmpty && internetAddress[0].rawAddress.isNotEmpty) {
+    return true;
+  } else {
+    return false;
+  }
+}
