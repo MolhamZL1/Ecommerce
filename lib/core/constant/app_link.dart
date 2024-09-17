0f7c7d6e5d50c51e28a947a42507e1cc830ac498
molhamsheikh <molhamsa49@gmail.com>
2024-09-17 12:18:46 +0300

    add app linksd

---
+++ lib/core/constant/app_link.dart
@@ -0,0 +1,13 @@
+class AppLink {
+
+static const String server = "https://waelabohamza.com/ecommerce"; 
+// 
+static const String test = "$server/test.php";
+
+// ================================= Auth ========================== //
+
+static const String signUp = "$server/auth/signup.php";
+
+
+
+}
